6ac91798147474683c7dea663d35b4d78096e3dc
indra saputra idrus <indrasaputraidrus@gmail.com>
2025-01-04 07:58:12 +0000

    refactor form input

--- src/lib/components/forms.svelte
+++ src/lib/components/forms.svelte
@@ -5,32 +5,45 @@
 		maxWidth: number;
 		maxHeight: number;
 	};
-
 	let { maxHeight, maxWidth }: Props = $props();
 
-	const setWidth = (v: number) => {
-		if (v > maxWidth) v = maxWidth;
-		if (v < 0) v = 0;
-		if (store.cropBox.lockRatio)
-			if (v > maxHeight) {
-				store.cropBox.offsetHeight = maxHeight;
-			} else {
-				store.cropBox.offsetHeight = v;
-			}
+	const isLockRatio = $derived(store.cropBox.lockRatio);
+	let width: null | number = $state(store.cropBox.offsetWidth);
+	let height: null | number = $state(store.cropBox.offsetHeight);
+
+	const setWidth = (v: number | null) => {
+    if(v === null) {
+      width = v
+      height = isLockRatio ? v : height
+      return
+    }
+
+		v = Math.max(0, Math.min(v, maxWidth));
 		store.cropBox.offsetWidth = v;
+		width = v;
+
+		if (isLockRatio) {
+			height = Math.min(v, maxHeight);
+			store.cropBox.offsetHeight = height;
+		}
 	};
 
-	const setHeight = (v: number) => {
-		if (v > maxHeight) v = maxHeight;
-		if (v < 0) v = 0;
-		if (store.cropBox.lockRatio)
-			if (v > maxWidth) {
-				store.cropBox.offsetWidth = maxWidth;
-			} else {
-				store.cropBox.offsetWidth = v;
-			}
+	const setHeight = (v: number | null) => {
+		if (v === null) {
+			height = v;
+			width = isLockRatio ? v : width;
+			return;
+		}
+
+		v = Math.max(0, Math.min(v, maxHeight));
 
 		store.cropBox.offsetHeight = v;
+		height = v;
+
+		if (isLockRatio) {
+			width = Math.min(v, maxWidth);
+			store.cropBox.offsetWidth = width;
+		}
 	};
 </script>
 
@@ -40,8 +53,8 @@
 		<input
 			class="border border-black p-2 w-full text-primary"
 			type="number"
-			bind:value={() => store.cropBox.offsetWidth, setWidth}
 			placeholder="width"
+			bind:value={() => width, setWidth}
 		/>
 	</label>
 	<label class="flex flex-col w-full">
@@ -49,7 +62,7 @@
 		<input
 			class="border border-black p-2 text-primary"
 			type="number"
-			bind:value={() => store.cropBox.offsetHeight, setHeight}
+			bind:value={() => height, setHeight}
 			placeholder="height"
 		/>
 	</label>
